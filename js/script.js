55ff267a7d007dd8540e504547c16f1de28131e2
sorzy <brisssou07@gmail.com>
2022-11-10 16:29:54 +0100

    letsgo

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+
+$('a[href*=#about]').click(function(event){
+  $('html, body').animate({
+      scrollTop: $( $.attr(this, 'href') ).offset().top
+  }, 500);
+  event.preventDefault();
+});
+
+
